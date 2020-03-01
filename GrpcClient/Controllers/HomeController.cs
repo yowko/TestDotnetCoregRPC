001ada9336e6a107f576cf2d83a9d3c1ad418cd9
Yowko Tsai <yowko0729@gmail.com>
2020-03-01 22:08:59 +0800

    Add project files.

---
+++ GrpcClient/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GrpcClient.Models;
+using GrpcMessges;
+
+namespace GrpcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly GrpcMessges.Greeter.GreeterClient _client;
+
+        public HomeController(ILogger<HomeController> logger, GrpcMessges.Greeter.GreeterClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var reply = await _client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
